3a7372e8f307a227bb9a713e987f27c8035cdb18
John Gutierrez <djterez88@gmail.com>
2022-09-12 19:58:01 -0700

    Created tests for custom user permission for updating a post.

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,13 +1,26 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissions, IsAuthenticatedOrReadOnly
 # Create your views here.
 
+# making custom permissions, to only allow logged in users to edit/delete own posts
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is restricted to the author only.'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        # matching post author with logged in user
+        return obj.author == request.user
+
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     # post_objects is from Model Manager of Post model
     queryset = Post.post_objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.post_objects.all()
     serializer_class = PostSerializer
